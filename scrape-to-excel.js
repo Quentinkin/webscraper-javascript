304a97dae66e7bf88711e51e74014913add44e3e
Raphael Rodriguez <92298204+Quentinkin@users.noreply.github.com>
2024-05-22 15:55:50 +0200

    Add const axios cheerios and xlsx and example url

---
+++ scrape-to-excel.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const xlsx = require("xlsx");
+
+// URL of the page we want to scrape
+const url = "https://webscraper.io/test-sites/e-commerce/allinone";
